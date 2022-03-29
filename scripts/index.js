fe30fd33fe22198c96f5a0da46ede923a9d36e62
Stella-de-mario <irina17otr@gmail.com>
2022-03-29 19:25:46 +0500

    first version

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let popupElement = document.querySelector('.popup');
+let editBtn = document.querySelector('.profile__edit-button');
+let closeBtn = popupElement.querySelector('.popup__close-button');
+let nameInput = popupElement.querySelector('input[name=name]');
+let professionInput = popupElement.querySelector('input[name=profession]');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+let formElement = popupElement.querySelector('.popup__container');
+
+
+function openPopup() {
+  popupElement.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  professionInput.value = profileProfession.textContent;
+}
+
+function closePopup() {
+  popupElement.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileProfession.textContent = professionInput.value;
+
+  closePopup();
+};
+
+editBtn.addEventListener('click', openPopup);
+
+closeBtn.addEventListener('click', closePopup);
+
+formElement.addEventListener('submit', formSubmitHandler);
