473a822c15c72e6513c69c1caa3ce946089ba88e
Stella-de-mario <irina17otr@gmail.com>
2022-03-30 14:11:12 +0500

    add js

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,21 @@
 let popupElement = document.querySelector(".popup");
 let editBtn = document.querySelector(".profile__edit-button");
 let closeBtn = popupElement.querySelector(".popup__close-button");
-let nameInput = document.querySelector("#user-name");
-let professionInput = document.querySelector("#user-profession");
+let nameInput = popupElement.querySelector('#user-name');
+let professionInput = popupElement.querySelector('#user-profession');
 let profileName = document.querySelector(".profile__name");
 let profileProfession = document.querySelector(".profile__profession");
-// let formElement = popupElement.querySelector(".popup__container");
+let formElement = popupElement.querySelector(".popup__container");
 
-function togglePopup() {
+function openPopup() {
   nameInput.value = profileName.textContent;
   professionInput.value = profileProfession.textContent;
 
-  popupElement.classList.toggle("popup_opened");
+  popupElement.classList.add("popup_opened");
+}
+
+function closePopup() {
+  popupElement.classList.remove("popup_opened");
 }
 
 function formSubmitHandler(evt) {
@@ -20,11 +24,11 @@ function formSubmitHandler(evt) {
   profileName.textContent = nameInput.value;
   profileProfession.textContent = professionInput.value;
 
-  togglePopup();
+  closePopup();
 }
 
-editBtn.addEventListener("click", togglePopup);
+editBtn.addEventListener("click", openPopup);
 
-closeBtn.addEventListener("click", togglePopup);
+closeBtn.addEventListener("click", closePopup);
 
 formElement.addEventListener("submit", formSubmitHandler);
