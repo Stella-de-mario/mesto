0885d21960bbe115e482b0f9a498f735aeb6f3a6
Stella-de-mario <irina17otr@gmail.com>
2022-07-01 13:45:34 +0500

    add class save-button_deletion

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -12,7 +12,7 @@ export default class PopupWithConfirmation extends Popup {
     
       setEventListeners() {
         super.setEventListeners();
-        this._popupConfirmation = this._popup.querySelector('.popup__save-button_disabled');
+        this._popupConfirmation = this._popup.querySelector('.popup__save-button_deletion');
         this._popupConfirmation.addEventListener('click', () => {
           this._handleSubmit(this._deleteCard); 
         }); 
